ffa5ef1c3ddc1bc3324e718791733be4c8e31276
Kimalu <zlf112358@163.com>
2012-06-09 15:25:14 +0800

    test

---
+++ spring_0100/src/com/Kimalu/test/Test.java
@@ -0,0 +1,14 @@
+package com.Kimalu.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kimalu
+ * Date: 12-6-9
+ * Time: 下午3:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("11111");
+    }
+}
